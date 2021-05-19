0ebbf797051593c7573d25edbe9a9c8880c324ac
Blaz Vrataric <online.blaz@gmail.com>
2021-05-19 09:14:35 +0200

    console log london weather

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import "./style.css";
 
-async function getComponent() {
-  const element = document.createElement("div");
-  const { default: _ } = await import("lodash");
+// CALL BY CITY NAME
+// api.openweathermap.org/data/2.5/weather?q={city name}&units=metric&appid={API key}
+// API KEY
+// 33eaa4fb488a76dcd98c62d852f4131f
+// TRY THIS: http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=33eaa4fb488a76dcd98c62d852f4131f
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-getComponent().then((component) => {
-  document.body.appendChild(component);
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=33eaa4fb488a76dcd98c62d852f4131f",
+  {
+    mode: "cors",
+  }
+)
+.then(function(response) {
+  return response.json();
+})
+.then(function(response) {
+  console.log(response);
 });
