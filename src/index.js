e6372ccb1ef04d83b23c233262dea5a6846740dd
Blaz Vrataric <online.blaz@gmail.com>
2021-05-23 08:38:32 +0200

    api data processed into new object

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ import "./style.css";
 // TRY THIS: http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=33eaa4fb488a76dcd98c62d852f4131f
 // npm run build
 
+let currentWeather;
+
+class Weather {
+  constructor(data) {
+    this.main = data.weather[0].main; // Clouds, Clear
+    this.temp = data.main.temp;
+    this.feels_like = data.main.feels_like;
+    this.humidity = data.main.humidity;
+    this.speed = data.wind.speed;
+    this.deg = data.wind.deg;
+    this.gust = data.wind.gust;
+    this.clouds = data.clouds.all;
+    this.sunrise = new Date(data.sys.sunrise * 1000);
+    this.sunset = new Date(data.sys.sunset * 1000);
+    this.name = data.name;
+  }
+}
+
 async function getWeather(name) {
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=33eaa4fb488a76dcd98c62d852f4131f`,
@@ -16,6 +34,8 @@ async function getWeather(name) {
   );
   const weatherData = await response.json();
   console.log(weatherData);
+  currentWeather = new Weather(weatherData);
+  console.log(currentWeather);
 }
 
-getWeather('koper');
+getWeather("radenci");
