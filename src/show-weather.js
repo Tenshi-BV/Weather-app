f28e9aabb0e02a4f1a2208b4b5e96140b361a394
Blaz Vrataric <online.blaz@gmail.com>
2021-06-03 08:32:48 +0200

    innerHTML dom lines attributed to correct elements

--- src/show-weather.js
+++ src/show-weather.js
@@ -12,26 +12,33 @@ const showWeather = (weather) => {
 
   const main = document.createElement("div");
   main.setAttribute("id", "main");
+  main.textContent = 'Hey';
+  // main.innerHTML = `<h2>${weather.name}</h2><img src="http://openweathermap.org/img/wn/${weather.icon} alt="weather icon for ${weather.main}><p>${weather.main}</p>`;
   container.appendChild(main);
 
   const temp = document.createElement("div");
   temp.setAttribute("id", "temp");
+  temp.innerHTML = ``;
   container.appendChild(temp);
 
   const humi = document.createElement("div");
   humi.setAttribute("id", "humi");
+  humi.innerHTML = ``;
   container.appendChild(humi);
 
   const wind = document.createElement("div");
   wind.setAttribute("id", "wind");
+  wind.innerHTML = ``;
   container.appendChild(wind);
 
   const cloud = document.createElement("div");
   cloud.setAttribute("id", "cloud");
+  cloud.innerHTML = ``;
   container.appendChild(cloud);
 
   const sun = document.createElement("div");
   sun.setAttribute("id", "sun");
+  sun.innerHTML = ``;
   container.appendChild(sun);
 
   /*
